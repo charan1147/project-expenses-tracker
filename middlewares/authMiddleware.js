15570f0893e1ccb288c1faa491ffeffecd4729ae
charan1147 <charanc1147@gmail.com>
2025-07-31 16:37:28 +0530

    first

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,13 @@ import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
 
     if (!token) {
       return res
